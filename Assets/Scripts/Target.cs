a8fd9fe81426979a2a63e4045ce40e47a3238c66
CasillasCesar <78441226+CasillasCesar@users.noreply.github.com>
2026-01-13 00:49:53 -0600

    Toss objects randomly in the air

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -2,10 +2,17 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private Rigidbody targetRb;
     void Start()
     {
-        
+        targetRb = GetComponent<Rigidbody>();
+        // Add an upward force multiplied by a randomized speed
+        targetRb.AddForce(Vector3.up * Random.Range(12,16), ForceMode.Impulse);
+        // Add a torque with randomized xyz values
+        targetRb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10), ForceMode.Impulse);
+        // Set the position with a randomized X value
+        transform.position = new Vector3(Random.Range(-4,4), -6);
     }
 
     // Update is called once per frame
